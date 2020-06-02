488a457ab59b69b5087c615ba716fb0b828feee6
zhoulucong0611 <zhoulucong0611@163.com>
2020-06-02 10:27:33 +0800

    修改分享功能

--- js/common_v5.js
+++ js/common_v5.js
@@ -1,16 +1,8 @@
 
-// $(window).on('load',function () {
-//         // InitWechatJSSDK();
-// })
-function wxShareConfig(val) {
-var baseLink = ''
-  if (val) {
-    baseLink = 'https://cdn.indata3.com/yarnexpo/share.html?tel=' + val
-  }
-  return baseLink
-}
-function InitWechatJSSDK(val) {
-    var baseLinkUrl = wxShareConfig(val)
+$(window).on('reday',function () {
+   InitWechatJSSDK();
+})
+function InitWechatJSSDK() {
     var url = window.location.href;
     // var data = {"appId":"wx19a6518b60fbdf06","nonceStr":"pZ2iL2OSMl6jI1Pj","timestamp":1591000057,"url":"http:\/\/cdn.indata3.com\/wechat\/wxSign.php","signature":"ce34190f3798f7c2b07d9bc2c1c82fcf507123ce","rawString":"jsapi_ticket=sM4AOVdWfPE4DxkXGEs8VG9z8TkOGp5HQj1w1CmVUMwQoDRy6V-22TLf1EfIrHhH81v2cUMOrpDb6hmeTwmqqg&noncestr=pZ2iL2OSMl6jI1Pj&timestamp=1591000057&url=http:\/\/cdn.indata3.com\/wechat\/wxSign.php"}
     $.ajax({
@@ -43,33 +35,6 @@ function InitWechatJSSDK(val) {
             });
             wx.ready(function () {
                 console.log('信息验证成功')
-                wx.onMenuShareAppMessage({
-                    title:'大湾区国际纺织纱线博览会', // 分享标题
-                    desc: 'Grrater Bay Area International Trade Fair for Fibres and Yarns', // 分享描述
-                    link: baseLinkUrl, //location.href	 分享链接
-                    imgUrl:'https://cdn.indata3.com/yarnexpo/image/sharelogo.png',
-                    type: '', // 分享类型,music、video或link，不填默认为link
-                    dataUrl: '', // 如果type是music或video，则要提供数据链接，默认为空
-                    success: function () {
-                        //alert("share ok!"); http://cdn.indata3.com/images/img/20180628/1530192197_60.jpeg
-                    },
-                    cancel: function () {
-                        // 用户取消分享后执行的回调函数
-                        //alert("share cancel!");
-                    }
-                });
-                wx.onMenuShareTimeline({
-                    title: '大湾区国际纺织纱线博览会', // 分享标题
-                    link: baseLinkUrl, // 分享链接
-                    imgUrl: 'https://cdn.indata3.com/yarnexpo/image/sharelogo.png', // 分享图标
-                    success: function () {
-                        // 用户确认分享后执行的回调函数
-                    },
-                    cancel: function () {
-                        // 用户取消分享后执行的回调函数
-                    }
-                });
-
                 wx.hideAllNonBaseMenuItem();
                 wx.showMenuItems({
                     menuList: ["menuItem:share:timeline", "menuItem:favorite", "menuItem:share:appMessage"] // 要显示的菜单项，所有menu项见附录3
